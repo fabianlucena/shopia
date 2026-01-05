aa382aadadd42cab4efeca471c513d01497f45aa
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-01-05 14:55:15 -0300

    feat: add event handlers for input and change events in Text component

--- frontend-shopia/src/components/controls/Text.svelte
+++ frontend-shopia/src/components/controls/Text.svelte
@@ -1,11 +1,23 @@
 <script>
   let {
     value = $bindable(''),
+    oninput = null,
+    onInput = null,
+    onchange = null,
+    onChange = null,
     ...props
   } = $props();
 </script>
 
 <input
   bind:value
+  oninput={(evt) => {
+    oninput?.(evt);
+    onInput?.(evt);
+  }}
+  onchange={(evt) => {
+    onchange?.(evt);
+    onChange?.(evt);
+  }}
   {...props}
 />
