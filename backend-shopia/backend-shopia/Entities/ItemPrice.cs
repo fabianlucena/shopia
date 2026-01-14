6582c0c30002cc1e7a67166ce059a7fab7b5385d
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-01-14 12:47:20 -0300

    feat: add ItemPrice entity with properties for ItemId and Price

---
+++ backend-shopia/backend-shopia/Entities/ItemPrice.cs
@@ -0,0 +1,21 @@
+﻿using RFService.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using RFService.Attributes;
+
+namespace backend_shopia.Entities
+{
+    [Table("ItemsPrices", Schema = "shopia")]
+    public class ItemPrice
+        : EntityCreatedAt
+    {
+        [Required]
+        [ForeignKey("Item")]
+        public Int64 ItemId { get; set; } = default;
+        public Item? Item { get; set; } = default;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
