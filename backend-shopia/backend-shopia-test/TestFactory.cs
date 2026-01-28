9509042c4dd2ff46a8f8afb468753f8366d934d0
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-01-27 21:51:11 -0300

    feat: add test project and initial test cases for login functionality

---
+++ backend-shopia/backend-shopia-test/TestFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using backend_shopia;
+
+namespace backend_shopia_test
+{
+    public class TestFactory : WebApplicationFactory<Program>
+    {
+        public TestFactory()
+        {
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Test");
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Reemplazar la BDD real por la de testing
+                //services.RemoveAll<DbContextOptions<MyDbContext>>();
+                //services.AddDbContext<MyDbContext>(options =>
+                //options.UseSqlServer("connection-string-test"));
+            });
+        }
+    }
+}
