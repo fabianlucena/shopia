c1090772c6ee9d511069f3e88b760a9d701f0422
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-02-03 12:28:59 -0300

    feat: refactor LoginTest to check success and fail login

--- backend-shopia/backend-shopia-test/LoginTest.cs
+++ backend-shopia/backend-shopia-test/LoginTest.cs
@@ -1,4 +1,7 @@
-﻿namespace backend_shopia_test
+﻿using System.Text;
+using System.Text.Json;
+
+namespace backend_shopia_test
 {
     [TestClass]
     public class LoginTest
@@ -9,12 +12,39 @@
         public void TestInit()
         {
             client = TestFactory.Singleton.CreateClient();
+            Console.WriteLine(client.BaseAddress);
         }
 
         [TestMethod]
         public async Task Login_returns_ok()
         {
-            var result = await client.GetAsync("/api/login");
+            var body = new
+            {
+                username = "admin",
+                password = "1234"
+            };
+
+            var json = JsonSerializer.Serialize(body);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = await client.PostAsync("/api/v1/login", content);
+
+            Assert.AreEqual(200, (int)result.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Login_returns_fail()
+        {
+            var body = new
+            {
+                username = "admin",
+                password = "12341"
+            };
+
+            var json = JsonSerializer.Serialize(body);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = await client.PostAsync("/api/v1/login", content);
+
+            Assert.AreEqual(403, (int)result.StatusCode);
         }
     }
 }
