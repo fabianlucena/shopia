07869eeb7a9de48a83ef7b6bb7ddf37d6e490d2b
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-09-11 16:58:15 -0300

    refactor: reorganize Program class structure for improved readability and maintainability

--- backend-shopia/backend-shopia/Program.cs
+++ backend-shopia/backend-shopia/Program.cs
@@ -3,53 +3,52 @@ using RFAuth.Filters;
 using RFAuth.Middlewares;
 using RFHttpExceptionsL10n.Middlewares;
 
-namespace backend_shopia
+namespace backend_shopia;
+
+public partial class Program
 {
-    public partial class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+        if (builder.Environment.IsEnvironment("Test"))
         {
-            var builder = WebApplication.CreateBuilder(args);
-            
-            builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
-            if (builder.Environment.IsEnvironment("Test"))
-            {
-                builder.Configuration.AddJsonFile("appsettings.Test.json", optional: true, reloadOnChange: true);
-            }
-
-            // Add services to the container.
-            builder.ConfigureServices();
-
-            builder.Services.AddRouting(options => options.LowercaseUrls = true);
-
-            builder.Services.AddControllers(options =>
-            {
-                options.Filters.Add<AuthorizationFilter>();
-            });
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            app.UsePathBase("/api");
-            app.UseRouting();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseCors("allowAll");
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseMiddleware<EnableBodyBufferingMiddleware>();
-            app.UseMiddleware<AuthorizationMiddleware>();
-            app.UseMiddleware<HttpExceptionL10nMiddleware>();
-            
-            app.MapControllers();
-
-            app.Run();
+            builder.Configuration.AddJsonFile("appsettings.Test.json", optional: true, reloadOnChange: true);
         }
+
+        // Add services to the container.
+        builder.ConfigureServices();
+
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+        builder.Services.AddControllers(options =>
+        {
+            options.Filters.Add<AuthorizationFilter>();
+        });
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        app.UsePathBase("/api");
+        app.UseRouting();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseCors("allowAll");
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseMiddleware<EnableBodyBufferingMiddleware>();
+        app.UseMiddleware<AuthorizationMiddleware>();
+        app.UseMiddleware<HttpExceptionL10nMiddleware>();
+        
+        app.MapControllers();
+
+        app.Run();
     }
 }
